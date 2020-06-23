40e14be06b74b5dc180d3b420a2af306ce117d9b
Aimimashio <31083@ojs.ac.jp>
2020-06-23 15:22:51 +0900

    店舗毎の合計

--- SalesManagement/Program.cs
+++ SalesManagement/Program.cs
@@ -11,61 +11,54 @@ namespace SalesManagement
     {
         static void Main(string[] args)
         {
-            int total = 0; //全体の売り上げ金額集計用
-            int mochitotal = 0;
-            int manjutotal = 0;
-            int youkantotal = 0;
+            int[] totals = new int[4] { 0, 0, 0, 0 };
+            string[] shops = { "新宿店", "浅草店", "丸の内店", "横浜店" };
+
             SalesCounter sales = new SalesCounter(ReadSales("Sales.csv"));
+
+            //集計
             foreach (var item in sales._sales)
             {
-                if( item.ProductCategory.Equals("カステラ"))
+                for (int i = 0; i < shops.Length; i++)
                 {
-                    total += item.Amount;
+                    if (item.ShopName.Equals(shops[i]))
+                    {
+                        totals[i] += item.Amount;
+                        break;
+                    }
                 }
-                else if(item.ProductCategory.Equals("餅菓子"))
-                {
-                    mochitotal += item.Amount;
-                }
-                else if(item.ProductCategory.Equals("まんじゅう"))
-                {
-                    manjutotal += item.Amount;
-                }
-                else if(item.ProductCategory.Equals("羊羹"))
-                {
-                    youkantotal += item.Amount;
-                }
-
             }
 
-            Console.WriteLine("カステラ売り上げ：{0}円", total);
-            Console.WriteLine("餅菓子売り上げ：{0}円", mochitotal);
-            Console.WriteLine("まんじゅう店売り上げ：{0}円", manjutotal);
-            Console.WriteLine("羊羹売り上げ：{0}円", youkantotal);
-
+            //出力
+            for (int i = 0; i < shops.Length; i++)
+            {
+                Console.WriteLine($"{shops[i]}の売上合計：{ totals[i]}");
+            }
         }
 
-        //売り上げデータを読み込み、Saleオブジェクトのリストを返す
-        static List<Sale> ReadSales(string filePath)
-        {
-            //売り上げデータを入れるリストオブジェクトを生成
-            var sales = new List<Sale>();
-            //ファイルから読み込み
-            string[] lines = File.ReadAllLines(filePath); //データを持ってくる
-            //読み込んだ桁の数だけ繰り返す
-            foreach (string line in lines)
+            //売り上げデータを読み込み、Saleオブジェクトのリストを返す
+            static List<Sale> ReadSales(string filePath)
             {
-                string[] items = line.Split(',');
-
-                //オブジェクト初期化子
-                Sale sale = new Sale
+                //売り上げデータを入れるリストオブジェクトを生成
+                var sales = new List<Sale>();
+                //ファイルから読み込み
+                string[] lines = File.ReadAllLines(filePath); //データを持ってくる
+                                                              //読み込んだ桁の数だけ繰り返す
+                foreach (string line in lines)
                 {
-                    ShopName = items[0],
-                    ProductCategory = items[1],
-                    Amount = int.Parse(items[2])
-                };
-                sales.Add(sale);    //saleオブジェクトをリストに追加
+                    string[] items = line.Split(',');
+
+                    //オブジェクト初期化子
+                    Sale sale = new Sale
+                    {
+                        ShopName = items[0],
+                        ProductCategory = items[1],
+                        Amount = int.Parse(items[2])
+                    };
+                    sales.Add(sale);    //saleオブジェクトをリストに追加
+                }
+                return sales;
             }
-            return sales;
         }
     }
-}
+
