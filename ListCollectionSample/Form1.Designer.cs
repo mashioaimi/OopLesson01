99a20bf60382097ec372fd8fb78bc88b265d34f8
Aimimashio <31083@ojs.ac.jp>
2020-06-29 16:01:52 +0900

    車登録アプリ

--- ListCollectionSample/Form1.Designer.cs
+++ ListCollectionSample/Form1.Designer.cs
@@ -36,24 +36,29 @@
             this.tbMaker = new System.Windows.Forms.TextBox();
             this.dgvCarData = new System.Windows.Forms.DataGridView();
             this.btAdd = new System.Windows.Forms.Button();
+            this.btOpenImage = new System.Windows.Forms.Button();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.btClearImage = new System.Windows.Forms.Button();
+            this.ofdOpenImage = new System.Windows.Forms.OpenFileDialog();
+            this.btModify = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCarData)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Location = new System.Drawing.Point(50, 37);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(58, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "車名";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // tbName
             // 
             this.tbName.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.tbName.Location = new System.Drawing.Point(102, 34);
+            this.tbName.Location = new System.Drawing.Point(124, 30);
             this.tbName.Name = "tbName";
             this.tbName.Size = new System.Drawing.Size(243, 31);
             this.tbName.TabIndex = 1;
@@ -62,7 +67,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label2.Location = new System.Drawing.Point(12, 104);
+            this.label2.Location = new System.Drawing.Point(24, 134);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(84, 24);
             this.label2.TabIndex = 0;
@@ -72,7 +77,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label3.Location = new System.Drawing.Point(12, 163);
+            this.label3.Location = new System.Drawing.Point(31, 222);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(77, 24);
             this.label3.TabIndex = 0;
@@ -81,7 +86,7 @@
             // tbCategory
             // 
             this.tbCategory.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.tbCategory.Location = new System.Drawing.Point(102, 160);
+            this.tbCategory.Location = new System.Drawing.Point(124, 219);
             this.tbCategory.Name = "tbCategory";
             this.tbCategory.Size = new System.Drawing.Size(243, 31);
             this.tbCategory.TabIndex = 1;
@@ -89,7 +94,7 @@
             // tbMaker
             // 
             this.tbMaker.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.tbMaker.Location = new System.Drawing.Point(102, 101);
+            this.tbMaker.Location = new System.Drawing.Point(124, 131);
             this.tbMaker.Name = "tbMaker";
             this.tbMaker.Size = new System.Drawing.Size(243, 31);
             this.tbMaker.TabIndex = 1;
@@ -97,27 +102,82 @@
             // dgvCarData
             // 
             this.dgvCarData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvCarData.Location = new System.Drawing.Point(16, 209);
+            this.dgvCarData.Location = new System.Drawing.Point(16, 256);
+            this.dgvCarData.MultiSelect = false;
             this.dgvCarData.Name = "dgvCarData";
+            this.dgvCarData.ReadOnly = true;
             this.dgvCarData.RowTemplate.Height = 21;
-            this.dgvCarData.Size = new System.Drawing.Size(445, 186);
+            this.dgvCarData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvCarData.Size = new System.Drawing.Size(628, 186);
             this.dgvCarData.TabIndex = 2;
+            this.dgvCarData.Click += new System.EventHandler(this.dgvCarData_Click);
             // 
             // btAdd
             // 
             this.btAdd.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.btAdd.Location = new System.Drawing.Point(365, 109);
+            this.btAdd.Location = new System.Drawing.Point(580, 151);
             this.btAdd.Name = "btAdd";
-            this.btAdd.Size = new System.Drawing.Size(97, 82);
+            this.btAdd.Size = new System.Drawing.Size(97, 47);
             this.btAdd.TabIndex = 3;
             this.btAdd.Text = "登録";
             this.btAdd.UseVisualStyleBackColor = true;
+            this.btAdd.Click += new System.EventHandler(this.btAdd_Click);
+            // 
+            // btOpenImage
+            // 
+            this.btOpenImage.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btOpenImage.Location = new System.Drawing.Point(581, 37);
+            this.btOpenImage.Name = "btOpenImage";
+            this.btOpenImage.Size = new System.Drawing.Size(97, 51);
+            this.btOpenImage.TabIndex = 3;
+            this.btOpenImage.Text = "開く...";
+            this.btOpenImage.UseVisualStyleBackColor = true;
+            this.btOpenImage.Click += new System.EventHandler(this.btOpenImage_Click);
+            // 
+            // pbImage
+            // 
+            this.pbImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbImage.Location = new System.Drawing.Point(382, 30);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(193, 220);
+            this.pbImage.TabIndex = 4;
+            this.pbImage.TabStop = false;
+            // 
+            // btClearImage
+            // 
+            this.btClearImage.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btClearImage.Location = new System.Drawing.Point(581, 94);
+            this.btClearImage.Name = "btClearImage";
+            this.btClearImage.Size = new System.Drawing.Size(97, 51);
+            this.btClearImage.TabIndex = 3;
+            this.btClearImage.Text = "クリア";
+            this.btClearImage.UseVisualStyleBackColor = true;
+            this.btClearImage.Click += new System.EventHandler(this.btClearImage_Click);
+            // 
+            // ofdOpenImage
+            // 
+            this.ofdOpenImage.FileName = "openFileDialog1";
+            // 
+            // btModify
+            // 
+            this.btModify.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btModify.Location = new System.Drawing.Point(581, 204);
+            this.btModify.Name = "btModify";
+            this.btModify.Size = new System.Drawing.Size(96, 46);
+            this.btModify.TabIndex = 5;
+            this.btModify.Text = "変更";
+            this.btModify.UseVisualStyleBackColor = true;
+            this.btModify.Click += new System.EventHandler(this.btModify_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(489, 422);
+            this.ClientSize = new System.Drawing.Size(708, 454);
+            this.Controls.Add(this.btModify);
+            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.btClearImage);
+            this.Controls.Add(this.btOpenImage);
             this.Controls.Add(this.btAdd);
             this.Controls.Add(this.dgvCarData);
             this.Controls.Add(this.tbMaker);
@@ -129,6 +189,7 @@
             this.Name = "Form1";
             this.Text = "車登録アプリ";
             ((System.ComponentModel.ISupportInitialize)(this.dgvCarData)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -144,6 +205,11 @@
         private System.Windows.Forms.TextBox tbMaker;
         private System.Windows.Forms.DataGridView dgvCarData;
         private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.Button btOpenImage;
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.Button btClearImage;
+        private System.Windows.Forms.OpenFileDialog ofdOpenImage;
+        private System.Windows.Forms.Button btModify;
     }
 }
 
--- ListCollectionSample/Form1.cs
+++ ListCollectionSample/Form1.cs
@@ -12,13 +12,89 @@ namespace ListCollectionSample
 {
     public partial class Form1 : Form
     {
+        //車データを入れるバインディングリスト
+        BindingList<Car> _cars = new BindingList<Car>();
+
         public Form1()
         {
             InitializeComponent();
+            dgvCarData.DataSource = _cars;
+
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void btAdd_Click(object sender, EventArgs e)
         {
+            Car car = new Car()
+            {
+                Name = tbName.Text,
+                Maker = tbMaker.Text,
+                Category = tbCategory.Text,
+                carPic = pbImage.Image
+            };
+
+
+            _cars.Insert(0, car); //リストの先頭(インデックス0)へ追加
+            dgvCarData.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+
+            inputItemAllClear();
+
+        }
+
+        private void inputItemAllClear()
+        {
+            tbName.Clear();
+            tbMaker.Clear();
+            tbCategory.Clear();
+            pbImage.Image = null;
+        }
+
+        private void btOpenImage_Click(object sender, EventArgs e)
+        {
+            if (ofdOpenImage.ShowDialog() == DialogResult.OK)
+            {
+                //選択した画像をピクチャーボックスに表示
+                pbImage.Image = Image.FromFile(ofdOpenImage.FileName);
+                //ピクチャーボックスのサイズに画像を調整
+                pbImage.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+
+        private void btClearImage_Click(object sender, EventArgs e)
+        {
+            pbImage.Image = null; //クリックされたらbtImageを何も入っていない状態にする
+        }
+
+        private void dgvCarData_Click(object sender, EventArgs e)
+        {
+            //選択したレコードを取り出す
+            //データグリッドビューで選択した行のインデックスを元に
+            //BindingListのデータを取得する
+            Car selectedCar = _cars[dgvCarData.CurrentRow.Index];
+            tbName.Text = selectedCar.Name;
+            tbMaker.Text = selectedCar.Maker;
+            tbCategory.Text = selectedCar.Category;
+        }
+
+        private void btModify_Click(object sender, EventArgs e)
+        {
+            //Car selectedCar = _cars[dgvCarData.CurrentRow.Index];
+            //selectedCar.Name = tbName.Text;
+            //selectedCar.Maker = tbMaker.Text;
+            //selectedCar.Category = tbCategory.Text;
+            //dgvCarData.Refresh();  データグリッドビューの描画
+
+            //変更対象のレコード(オブジェクト)
+            Car modify = new Car() //中の処理を変更
+            {
+                Name = tbName.Text,
+                Maker = tbMaker.Text,
+                Category = tbCategory.Text,
+                carPic = pbImage.Image
+            };
+
+            _cars[dgvCarData.CurrentRow.Index] = modify; //格納された行の文字を変更
+
+            inputItemAllClear();
 
         }
     }
